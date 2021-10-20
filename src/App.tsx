8831425b8fa3bc48cc5a156f8f34b90bae89e6f7
ayush-shta <ayushshta@gmail.com>
2021-10-20 15:03:01 +0545

    Initial setup for react-navigation library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  return <></>;
+  return (
+    <NavigationContainer>
+      <></>
+    </NavigationContainer>
+  );
 };
 
 export default App;
