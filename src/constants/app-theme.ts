ffe04e08e8360d0dcc6cec502d3e74ffce5359fe
ayush-shta <ayushshta@gmail.com>
2021-10-21 16:47:40 +0545

    Add app theme color constants

---
+++ src/constants/app-theme.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: '#1D70B8',
+  background: '#F3F2EF',
+  secondaryGreen: '#39D300',
+  secondaryRed: '#FF1154',
+  secondaryAqua: '#92D5E8',
+  lightGray: '#959FAE',
+  darkGray: '#666666',
+  border: '#CED2D3',
+};
